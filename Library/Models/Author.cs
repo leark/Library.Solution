e52689c1762c91950de6e6349893f971bde42d60
Seung Lee <shl7@uw.edu>
2022-08-08 11:28:21 -0700

    Add Author, Book, and AuthorBook class
    Co-Authored-By: Joseph Jack <joseph.w.jack@gmail.com>

--- Library/Models/Author.cs
+++ Library/Models/Author.cs
@@ -2,15 +2,15 @@ using System.Collections.Generic;
 
 namespace Library.Models
 {
-  public class Book
+  public class Author
   {
-    public Book()
+    public Author()
     {
       this.JoinEntities = new HashSet<AuthorBook>();
     }
 
     public int BookId { get; set; }
-    public string Title { get; set; }
+    public string Name { get; set; }
 
     public virtual ICollection<AuthorBook> JoinEntities { get; }
   }
